749631229d2afde77b68474a0d7c0d6671c1215c
ayninur <nuralaynabdulqadir@gmail.com>
2020-07-22 11:32:18 -0400

    Get Even answered

--- unit_1/w02d2.js
+++ unit_1/w02d2.js
@@ -5,3 +5,9 @@
 // for (i = 0; i <= 20; i++)
 // console.log(i)
 // //////////////////
+// 2. Write a for loop that will log only the even numbers 0 through 200
+// 
+for (i = 0; i <= 200; i++) {
+    if (i % 2 === 0)
+    console.log(i + " is even")
+}
