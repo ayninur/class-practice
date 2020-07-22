e6d4067a97bf35ff1c017917ff3328b128e22528
ayninur <nuralaynabdulqadir@gmail.com>
2020-07-22 11:28:12 -0400

    Easy Going answered

---
+++ unit_1/w02d1.js
@@ -0,0 +1,121 @@
+// 1. How do we assign a value to a variable?
+// - We can assign a value to a variable by using the variable var and using the assign key or "=" and a unique identifying name
+//  1a. Which lines are valid? line 1 (let a = 0;) and line 2 (const b = 1;)
+// 2. How do we change the value of a variable?
+//  We change the value of a vairable by choosing a name for the variabl following it by the assignment key and what ever you want your new variable to be.
+// 3. How do we assign an existing value of a variable? 
+//  NOT SURE FIRI
+// Activity 1: 
+// 
+// >>>>>>>>Strings<<<<<<<<<<<<<
+// 
+// var firstVariable = 5
+
+// var firstVariable = secondVariable
+
+// var secondVariable = "a value";
+// console.log("what") **********Not SURE HOW CIRCLE BACK* ASK ON EXIT TICKET IF DONT GET ANSWER BY TONIGHT
+// 
+// >>>>>>>>>>>>>Combine it<<<<<<<<<<<<<<
+// 
+// 1. Create a variable called yourName and set it equal to your name as a string.
+// - Write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+//       ex: Hello, my name is Jean Valjean
+
+// var yourName = "Ayni Nur"
+
+// if (yourName === yourName){
+//     console.log("Hello, my name is " + yourName);
+// }
+// Booleans Activity
+// 1. a < b
+// 2. c < d
+// 3. 'Peanut' === 'Peanut'
+// 4. a < b > c;
+// 5. a === a > d
+// 6. 'e' == 'Jelly Bean' *CHECK*
+// 7. 48 == '48'
+// 8. 'e' !== 'Eh'
+// 
+//>>>>>>>>>>>>The Farm<<<<<<<<<<<<
+// 
+// 1. Write code that will print out "mooooo" if the variable animal is equal to cow.
+// 
+// var animal = "cow";
+// console.log("moooo");
+// 
+// 2. Change your code so that if the variable animal is anything other than a cow, your should print out "Hey! You're not a cow."
+// 
+// var animal = "bow";
+// 
+// if (animal == "cow"){
+//     console.log("mooooo")
+// } else{
+//     console.log("Hey! You're not a cow")
+// }
+// 
+// >>>>>>>>>>>>>>>Driver's Ed<<<<<<<<<<<<<<<<
+// 
+//   var personAge = 18
+
+//   if (personAge >= 16){
+//     console.log("Here are the keys")
+// } else{ 
+//     console.log("Sorry, you're too young.")
+// }
+// 
+//  >>>>>>>>>>>>>>Just Loop it<<<<<<<<<<<<<<<<<<<
+// 
+// 1a. Write code that will print out all the numbers in the range 0 - 10
+
+// for (let i = 0; i < 10; i++ ){
+//   console.log(i);
+// }
+
+// 1b. Write code that will print out all of the numbers in the range 500 - 50
+// 
+// for (let i = 500; i >= 50; i-- ){
+//   console.log(i)
+// }
+// 
+//  >>>>>>>>>>>>>>>>>>>>>> Let's get odd <<<<<<<<<<<<<<<<<<<<<<<<<
+// 
+//  1. Print out the odd numbers that are within the range of 1 - 100 & and adjust your code to add a message next to the odd number that says "is any odd number"
+
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 1) {
+//     console.log(i + " is an odd number");
+//   }
+// }
+// 
+// >>>>>>>>>>>>>>>>> Give me Five <<<<<<<<<<<<<<<<<<<<<<
+// 
+//  1. For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five.
+
+for (i = 0; i <= 100; i++){
+  if (i % 5 === 0) {
+    console.log(i + " I found a number. High five!")
+  } else if (i= 0; i < 100; i++){
+    if (i % 3 === 1) {
+      console.log("I found a number. Three is a crowd!")
+    }
+  }
+}
+//  CIRCLE BACK LATER
+
+
+
+
+
+
+
+ 
+
+
+ 
+
+
+
+
+
+
---
+++ unit_1/w02d2.js
@@ -0,0 +1,7 @@
+////////////////////////////////
+// Part II: JavaScript Reps
+////////////////////////////////
+// 1. Write a for loop that will log the numbers from 1 through 20
+// for (i = 0; i <= 20; i++)
+// console.log(i)
+// //////////////////
