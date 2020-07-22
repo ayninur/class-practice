eb38663df5a9f85d1b7fe4b5269c0dd2f1d696b9
ayninur <nuralaynabdulqadir@gmail.com>
2020-07-22 11:54:59 -0400

    Wild Wild Life

--- unit_1/w02d2.js
+++ unit_1/w02d2.js
@@ -26,14 +26,61 @@
 /////////////////////////////////
 
 //  Write a js application that logs all numbers from 1-100
-for (let i = 0; i <= 100; i++){
-    if (i % 3 === 0) {
-        console.log("Fizz");//if number divided by 3 log fizz
-    } else if (i % 5 === 0) {
-        console.log("Buzz!");//if divisible by 5 Buzz
-    } else if (i % 3 === 0 && i % 5 === 0){
-        console.log("FizzBuzz!");//if divisible by both FizzBuz
-    } else {
-        console.log("This number is divisible by neither––")
-    }
-}
+// for (let i = 0; i <= 100; i++){
+//     if (i % 3 === 0) {
+//         console.log("Fizz");//if number divided by 3 log fizz
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz!");//if divisible by 5 Buzz
+//     } else if (i % 3 === 0 && i % 5 === 0){
+//         console.log("FizzBuzz!");//if divisible by both FizzBuz
+//     } else {
+//         console.log("This number is divisible by neither––")
+//     }
+// }
+
+/////////////////////////////////
+////// Wild Wild Life
+/////////////////////////////////
+
+// 1. Plantee just had a birthday; change Plantee's array to reflect her being a year older
+
+// const plantee = ["plantee", "plant", 5000, "Mordor"] 
+// plantee[2] = 5001;
+// console.log(plantee)
+
+//  2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City"
+
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// wolfy[3] = "Gotham City"
+// console.log(wolfy)
+
+//  3. Give D'Art a second hometown by adding "Hawkins"
+
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// dart.push('Hawkins')
+// console.log(dart)
+
+// 4. Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+wolfy[0] = "Gameboy"
+console.log(wolfy)
+
+
+
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// // const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// // *****************
+// //  1. Plantee just had her birthday; change Plantees array to reflect her being a year older
+// const plantee = ["plantee", "plant", 5000, "Mordor"] {
+// console.log(3)} not sureeeeeee///////////////check this outttt
+// // ******************
+// 2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// wolfy[3] = "Gotham City"
+// console.log(wolfy)
