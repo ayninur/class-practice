bb463a63c8483ab62b03ce868e2480c8ea3394b2
ayninur <nuralaynabdulqadir@gmail.com>
2020-07-22 12:45:56 -0400

    Where is Waldo answered

--- unit_1/w02d2.js
+++ unit_1/w02d2.js
@@ -134,13 +134,21 @@
 /////////////////////////////////
 
 // With the following multi-dimensional array
+
 const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
                     ["Lucinda", "Jacc", "Neff", "Snoop"],
                     ["Petunia", ["Baked Goods", "Waldo"]]];
+
 // Remove Eggbert (hint look at the slice/splice method(s))
 
-whereIsWaldo.splice(0,1)
+whereIsWaldo.splice(1,1)
 console.log(whereIsWaldo);
 
 // Change "Neff" to "No One"
+
+whereIsWaldo[1][2] = "No One";
+console.log(whereIsWaldo[1][2]);
+
 // Access and console.log "Waldo"
+
+console.log(whereIsWaldo[2][1][1]);
