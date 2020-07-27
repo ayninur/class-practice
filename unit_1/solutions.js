99b3f567c7e5df3afae9bef8dfa34dadfb9d63a2
ayninur <nuralaynabdulqadir@gmail.com>
2020-07-27 01:13:05 -0400

    palindrome completed

--- unit_1/solutions.js
+++ unit_1/solutions.js
@@ -12,4 +12,24 @@
 // will cause reactions in your script. Additionally, return statements may not always show on the console where as console.logs do
 // 
 
+/////////////////////////////////////////////////////
+/////// Palindrome
+/////////////////////////////////////////////////////
+
+// Write a function checkPalindrome that accepts a single argument, a string. The function should return true (Boolean) if the string 
+// is a palindrome, false if it is not. Make sure your function will give the correct answer for words with capital letters.
+
+const checkPalindrome = (string) => {           //Write a function checkPalindrome that accepts a single argument a string.
+    // The function should return true if the string is palindrome a plaindrome = the word backwards
+    sting = string.toUpperCase()
+    let stringReversed = string.split("").reverse().join("");//describing what palindrome is
+    if (string === stringReversed) {
+        return true
+    }
+    return false;
+}
+console.log(checkPalindrome("badab"));
+console.log(checkPalindrome("Ayni"))
+
+
 
