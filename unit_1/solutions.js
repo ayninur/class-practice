75c37d4d30dd28c7f33b87764bda0714640a8ac4
ayninur <nuralaynabdulqadir@gmail.com>
2020-07-26 23:52:16 -0400

    verbal questions completed

--- unit_1/solutions.js
+++ unit_1/solutions.js
@@ -1,1 +1,15 @@
+/////////////////////////////////////////////////////
+/////// Verbal Questions
+/////////////////////////////////////////////////////
+
+//  1. What is the difference between a parameter and an arguement?
+// 
+// The arguement is the input and the parameter is how the input is represented in the function.
+// 
+// 2. Within a function, what is the difference between return and console.log?
+// 
+// Some of the differences between the two is console.log does not interrupt your flow where as return
+// will cause reactions in your script. Additionally, return statements may not always show on the console where as console.logs do
+// 
+
 
