56e220fc5e14d226840baeebbd8a4d48051d715c
ayninur <nuralaynabdulqadir@gmail.com>
2020-08-03 00:48:12 -0400

    objectception complete

--- unit_1/jsdatatypes.js
+++ unit_1/jsdatatypes.js
@@ -74,54 +74,84 @@
 //// Crazy Object
 //////////////////////////////////////////////////////////
 
-const crazyObject = {
-    taco: [{meat: 'steak',
-           cheese: ['panela', 'queso', 'chihuahua']},
-           {meat: 'chicken',
-            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
-          ],
-    larry: {
-      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
-      characters: [{
-        name: "Jeff",
-        occupation: "manager"
-      },
-      {
-        name: "funkhauser",
-        occupation: "tv dude"
-      },
-      {
-        name: "susie",
-        occupation: "jeffs wife",
-        favourtieHobby: "Swearing at Larry and Jeff"
-      },
-      ]
-    }
-  }
+// const crazyObject = {
+//     taco: [{meat: 'steak',
+//            cheese: ['panela', 'queso', 'chihuahua']},
+//            {meat: 'chicken',
+//             salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+//           ],
+//     larry: {
+//       nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//       quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//       characters: [{
+//         name: "Jeff",
+//         occupation: "manager"
+//       },
+//       {
+//         name: "funkhauser",
+//         occupation: "tv dude"
+//       },
+//       {
+//         name: "susie",
+//         occupation: "jeffs wife",
+//         favourtieHobby: "Swearing at Larry and Jeff"
+//       },
+//       ]
+//     }
+//   }
 
 // 1. "omg my mouth is burning"
 
-console.log(crazyObject.taco[1].salsa[5])
+// console.log(crazyObject.taco[1].salsa[5])
 
 // 2. "Pretty pretty prettayyyyy good"
 
-console.log(crazyObject.larry.quotes[0])
+// console.log(crazyObject.larry.quotes[0])
 
 // 3. "Swearing at Larry and Jeff"
 
-console.log(crazyObject.larry.characters[2].favourtieHobby)
+// console.log(crazyObject.larry.characters[2].favourtieHobby)
 
 // 4. "Chicken Teriyaki Boyyyyyy"
 
-console.log(crazyObject.larry.nicknames[1])
+// console.log(crazyObject.larry.nicknames[1])
 
 // 5. The object the contains the name funkhauser
 
-console.log(crazyObject.larry.characters[1].name)
+// console.log(crazyObject.larry.characters[1].name)
 
 // 6. Add the quote "I'm trying to elevate small talk to medium talk" to Larry's quote array. Confirm that it was added.
 
-console.log(crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk"))
-console.log(crazyObject.larry.quotes)
+// console.log(crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk"))
+// console.log(crazyObject.larry.quotes)
 
+
+
+//////////////////////////////////////////////////////////
+//// Object-ception
+//////////////////////////////////////////////////////////
+
+const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                           }
+                        }
+                    }
+                }
+            }
+        }
+    }
+ }
+
+
+
+//  1. Change the value of limbo to null.
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo)
