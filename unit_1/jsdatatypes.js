84cfb019799ed69f30b1f3f4ccee7a3bb1ab8141
ayninur <nuralaynabdulqadir@gmail.com>
2020-08-03 00:36:45 -0400

    objectception complete

--- unit_1/jsdatatypes.js
+++ unit_1/jsdatatypes.js
@@ -49,23 +49,79 @@
 
 // 1. Make an array that holds all of the colors of the rainbow.
 
-const colorsOfRainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+// const colorsOfRainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 // 2. Write code that will access "blue" from the rainbow array.
 
-console.log(colorsOfRainbow[4])
+// console.log(colorsOfRainbow[4])
 
 // 3. Make an object that is called your name and holds the information about your favorite food, a hobby, 
 // the name of the town that you live in currently, and your favorite datatype.
 
-const ayni = {
-    Food: "chocolate",
-    hobby: "hiking",
-    hometown: "Boston",
-    datatype: "objects",
-}
+// const ayni = {
+//     Food: "chocolate",
+//     hobby: "hiking",
+//     hometown: "Boston",
+//     datatype: "objects",
+// }
 
-// 3. Write code that will access your hobby from the object that you just created.
+// 4. Write code that will access your hobby from the object that you just created.
 
-console.log(ayni.hobby)
+// console.log(ayni.hobby)
+
+
+//////////////////////////////////////////////////////////
+//// Crazy Object
+//////////////////////////////////////////////////////////
+
+const crazyObject = {
+    taco: [{meat: 'steak',
+           cheese: ['panela', 'queso', 'chihuahua']},
+           {meat: 'chicken',
+            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+          ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [{
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+      ]
+    }
+  }
+
+// 1. "omg my mouth is burning"
+
+console.log(crazyObject.taco[1].salsa[5])
+
+// 2. "Pretty pretty prettayyyyy good"
+
+console.log(crazyObject.larry.quotes[0])
+
+// 3. "Swearing at Larry and Jeff"
+
+console.log(crazyObject.larry.characters[2].favourtieHobby)
+
+// 4. "Chicken Teriyaki Boyyyyyy"
+
+console.log(crazyObject.larry.nicknames[1])
+
+// 5. The object the contains the name funkhauser
+
+console.log(crazyObject.larry.characters[1].name)
+
+// 6. Add the quote "I'm trying to elevate small talk to medium talk" to Larry's quote array. Confirm that it was added.
+
+console.log(crazyObject.larry.quotes.push("I'm trying to elevate small talk to medium talk"))
+console.log(crazyObject.larry.quotes)
 
