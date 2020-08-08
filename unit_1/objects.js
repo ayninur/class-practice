3673ac3199b0f41be6f2ce4fa7ba7cba19542730
ayninur <nuralaynabdulqadir@gmail.com>
2020-08-08 06:03:37 -0400

    created hamster class

--- unit_1/objects.js
+++ unit_1/objects.js
@@ -5,8 +5,6 @@
 
 // 1. Hamster Class
 
-
-
 class Hamster {
     constructor(owner, name, price) {
         this.owner = owner,
@@ -26,6 +24,57 @@ class Hamster {
         console.log(this.price);
     }
  
+}
+
+// 2. Person Class
+
+class Person {
+    constructor(name, age, height, weight, mood, hamsters, bankAccount) {
+        this.name = name,
+        this.age = 0,
+        this.height = 0,
+        this.weight = 0,
+        this.mood = 0,
+        this.hamsters = [],
+        this.bankAccount = 0
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getAge() {
+        return this.age
+    }
+
+    getWeight() {
+        return this.weight
+    }
+
+    greet() {
+        console.log(`this my ${this.name}`);
+    }
+    eat() {
+        this.weight++;
+        this.mood++
+    }
+    exercise() {
+        return this.weight--
+    }
+    ageUp() {
+        this.age++;
+        this.height++;
+        this.weight++;
+        this.mood--;
+        this.bankaccount += 10;
+    }
+    buyHamster() {
+        this.hamsters.push()
+        this.mood++;
+        this.bankAccount--;
+
+    }
+
 }
 
 
