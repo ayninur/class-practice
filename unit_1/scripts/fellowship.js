f07d834bf18f6af844d0bb9b40eca7abec2b3db1
ayninur <nuralaynabdulqadir@gmail.com>
2020-08-19 12:13:28 -0400

    Chapter 2 complete - Made the Hobbits

--- unit_1/scripts/fellowship.js
+++ unit_1/scripts/fellowship.js
@@ -74,16 +74,23 @@ const makeHobbits = () => {
 
   // Goal: display an unordered list of hobbits in the shire (which is the first article tag on the page)
 
-  // 1. create a 'ul'
+// 1. create a 'ul'
+const $theHobbits = $('<ul>').addClass('the-hobbits')
 
-  // 2. make each hobbit an li element and append it to the 'ul' you just created
+// 2. make each hobbit an li element and append it to the 'ul' you just created
     // hint: use the given 'hobbits' array and use a for loop
+// 3. also, give each hobbit (`li`) a class of "hobbit"
 
-  // 3. also, give each hobbit (`li`) a class of "hobbit"
-
-  // 4. append the ul to the shire
+for (i = 0; i < hobbits.length; i++) {
+    const $li = $('<li>').text(hobbits[i]).addClass('hobbit')
+    
+    // 4. append the ul to the shire
     // hint: get 'The-Shire' by using its id
-
+   $theHobbits.append($li)
+    $('#The-Shire').append($theHobbits)
+    
+    
+}
 };
 
 // COMMIT YOUR WORK
