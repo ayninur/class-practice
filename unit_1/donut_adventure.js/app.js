01d25fb9a3b9ad1f83a6c54c9a69b61d6846e601
ayninur <nuralaynabdulqadir@gmail.com>
2020-08-20 21:22:57 -0400

    created our hero dougie

--- unit_1/donut_adventure.js/app.js
+++ unit_1/donut_adventure.js/app.js
@@ -1,20 +1,25 @@
-class Hamster {
-    constructor (owner, name, price) {
-        this.owner = owner,
-        this.name = name,
-        this.price = 15
-    }
-//    methods
-        wheelRun() {
-            console.log("squeak squeak");
+class Hero {
+    constructor(name, health) {
+        this.name = name;
+        this.health = health;
+        this.weapons = {
+            sprinkleSpray: 5,
+            sugarShock: 10
         }
-        
-        eatFood() {
-            console.log("nibble nibble");
+        this.catchPhrases = [
+            `I'm fresher than day old pizza`,
+            `you can't count my calories`
+        ]
         }
-        
-        getPrice() {
-            console.log(this.price) 
+        talkSass() {
+            Math.floor(math.random(this.catchPhrases.length))
+        }
+        announceHealth() {
+            console.log(this.health)
+        }
+        fight() {
+            console.log("I'm ready to rumble")
         }
 }
 
+const dougie = new Hero("Dougie");
